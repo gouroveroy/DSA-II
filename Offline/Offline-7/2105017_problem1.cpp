5ec9e2051e2fd2035d56cc6d3a43d1aa092702ba
Gourove <2105017@ugrad.cse.buet.ac.bd>
2024-02-16 01:13:05 +0600

    successfull hashing after long pain

--- Offline/Offline-7/2105017_problem1.cpp
+++ Offline/Offline-7/2105017_problem1.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include "RandomWordGenerator.cpp"
-// #include "x.cpp"
-#include "y.cpp"
+#include "x.cpp"
+// #include "y.cpp"
 // #include "hashtable.cpp"
 
 int main()
@@ -10,7 +10,6 @@ int main()
     RandomWordGenerator rwg;
 
     vector<string> words = rwg.generateRandomWords(100, 5, 10);
-    // vector<string> words = {"abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz", "abc"};
 
     int count = 1;
     for (auto &word : words)
@@ -19,14 +18,5 @@ int main()
         ht.insert(word, count);
         count++;
     }
-    // string word;
-    // while (1)
-    // {
-    //     cin >> word;
-    //     cout << word << endl;
-    //     ht.insert(word, count);
-    //     count++;
-    // }
-    
     return 0;
 }
---
+++ Offline/Offline-7/z.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+bool isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int findNextPrime(int prime)
+{
+    int nextPrime = prime;
+    while (!isPrime(nextPrime))
+    {
+        nextPrime++;
+    }
+    return nextPrime;
+}
