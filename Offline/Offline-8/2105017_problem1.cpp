db4dd965ef95905459150d528c21c9eef8e8f80d
Gourove <2105017@ugrad.cse.buet.ac.bd>
2024-04-14 00:22:37 +0600

    offline 8

--- Offline/Offline-8/2105017_problem1.cpp
+++ Offline/Offline-8/2105017_problem1.cpp
@@ -45,15 +45,23 @@ int main()
     for (auto e : epsilon)
     {
         cout << "Rounded Instance with Eps: " << e << endl;
+        /* The line `double theta = (double)(e * v_max) / (double)(2 * n);` is calculating the value of
+        `theta` in the rounding experiment for a given epsilon value `e`. Here's a breakdown of the
+        calculation: */
         double theta = (double)(e * v_max) / (double)(2 * n);
 
         cout << "Theta: " << setprecision(15) << theta << endl;
 
+        /* The code snippet you provided is creating a vector `roundValues` of integers with a size of
+        `n + 1`. Then, it iterates over each element from 1 to `n` (inclusive) and calculates a
+        rounded value for each element based on the division of `value[i]` by `theta`. */
         vector<int> roundValues(n + 1);
         for (int i = 1; i <= n; i++)
         {
             roundValues[i] = ceil((double)(value[i] / theta));
         }
+        /* The code snippet you provided is part of a rounding experiment on the original knapsack
+        instance. Here's a breakdown of what it does: */
         int maxRoundValue = *max_element(roundValues.begin(), roundValues.end());
 
         maxRoundValue = maxRoundValue * n;
@@ -62,6 +70,9 @@ int main()
         cout << "Answer of reduced instance: " << reducedInstance.first << endl;
         cout << "Answer of reduced instance multiplied by theta: " << (double)(reducedInstance.first * theta) << endl;
 
+        /* The code snippet you provided is retrieving the indices of the selected items in the
+        knapsack for the reduced instance after rounding the values based on a specific epsilon
+        value. Here's a breakdown of what it does: */
         vector<int> e_indices = ks.getIndices();
         cout << "Indices: ";
         for (auto v : e_indices)
@@ -70,6 +81,9 @@ int main()
         }
         cout << endl;
 
+        /* This part of the code snippet is calculating and displaying information related to the
+        original knapsack instance after rounding up the values based on a specific epsilon value.
+        Here's a breakdown of what it does: */
         int originalInstance = 0;
         int usedWeight = 0;
         for (auto index : e_indices)
